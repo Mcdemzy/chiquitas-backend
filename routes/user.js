d77490f9ecaf80fe88887c8d2ba1e487fb491ef3
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-08-06 10:34:19 +0100

    Rewriting Code

--- routes/user.js
+++ routes/user.js
@@ -90,6 +90,7 @@ router.post("/login", async (req, res) => {
     });
 
     res.cookie("token", token, {
+      httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       maxAge: 3600000,
       sameSite: "lax",
